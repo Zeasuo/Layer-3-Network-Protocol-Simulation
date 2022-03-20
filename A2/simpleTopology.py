392962de688c6faae46b9580e17526b3a91ad1b6
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-20 04:25:12 -0400

    s

--- A2/simpleTopology.py
+++ A2/simpleTopology.py
@@ -1,6 +1,6 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Node, Controller, RemoteController
+from mininet.node import Node
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
@@ -16,47 +16,28 @@ The ip address of hosts and interfaces in this topology is static.
 """
 
 
-def MyTopo():
-    net = Mininet(controller=RemoteController)
+class SimpleTopo(Topo):
+    def __init__(self, **params):
+        Topo.__init__(self)
 
-    info('***Adding controller\n')
+        # Add hosts and switches
+        r1 = self.addHost('r1', ip='10.1.0.1/24')
+        h1 = self.addHost('h1', ip='10.1.0.1', defaultRoute='via 10.1.0.1')
+        h2 = self.addHost('h2', ip='10.1.0.2', defaultRoute='via 10.1.0.1')
+        h3 = self.addHost('h3', ip="10.100.0.1", defaultRoute='via 10.100.0.1')
+        h4 = self.addHost('h4', ip="10.100.0.2", defaultRoute='via 10.100.0.1')
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
 
-    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
+        # Add links based on the above diagram
+        self.addLink(s1, r1, intfName2='r1-eth1', params2={'ip': '10.1.0.1/24'})
+        self.addLink(s2, r1, intfName2='r1-eth2', params2={'ip': '10.100.0.1/24'})
 
-    # Add hosts and switches
-    r1 = net.addHost('r1', ip='10.1.0.1/24')
-    h1 = net.addHost('h1', ip='10.1.0.1', defaultRoute='via 10.1.0.1')
-    h2 = net.addHost('h2', ip='10.1.0.2', defaultRoute='via 10.1.0.1')
-    h3 = net.addHost('h3', ip="10.100.0.1", defaultRoute='via 10.100.0.1')
-    h4 = net.addHost('h4', ip="10.100.0.2", defaultRoute='via 10.100.0.1')
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
 
-    # Add links based on the above diagram
-    net.addLink(s1, r1, intfName2='r1-eth1', params2={'ip': '10.1.0.1/24'})
-    net.addLink(s2, r1, intfName2='r1-eth2', params2={'ip': '10.100.0.1/24'})
-
-    net.addLink(h1, s1)
-    net.addLink(h2, s1)
-
-    net.addLink(h3, s2)
-    net.addLink(h4, s2)
-
-    info('***Starting network\n')
-    net.build()
-
-    c0.start()
-
-    s1.start([c0])
-
-    s2.start([c0])
-    info('***Running CLI\n')
-    CLI(net)
-
-    info('*** Stopping network')
-    net.stop()
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
 
 
-if __name__ == "__main__":
-    setLogLevel('info')
-    MyTopo()
+topos = {'mytopo': (lambda: SimpleTopo())}
