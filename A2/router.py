0a1509038d125906065757b1810fb77048f12e2d
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-20 20:56:56 -0400

    s

--- A2/router.py
+++ A2/router.py
@@ -1,6 +1,9 @@
 import socket
+import netifaces as ni
 
 
 if __name__ == "__main__":
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print(socket.gethostbyname(socket.gethostname()))
+    ni.ifaddresses('eth0')
+    ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+    print(ip)
