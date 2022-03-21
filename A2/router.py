2c52b0058619b5a85c95cdcbff709e26ae024384
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-21 04:17:53 -0400

    s

--- A2/router.py
+++ A2/router.py
@@ -15,8 +15,15 @@ if __name__ == "__main__":
         if intf != 'lo':
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
             listen_sockets[intf] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            listen_sockets[intf].setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             listen_sockets[intf].bind((ip, 9000))
 
     while True:
-        data, address = listen_sockets['r1-eth1'].recvfrom(1024)
-        print(data, address)
+        input_sockets = listen_sockets.values()
+        readable, writable, exceptional = select.select(input_sockets,
+                                                        [],
+                                                        [])
+        print("found")
+        for s in readable:
+            data, address = s.recvfrom(1024)
+            print(data, address)
