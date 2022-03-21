09950a9222a8ac4a3ea277e8f07e07c310aec9ab
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-21 03:48:01 -0400

    s

--- A2/router.py
+++ A2/router.py
@@ -15,7 +15,6 @@ if __name__ == "__main__":
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
             listen_sockets[intf] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             listen_sockets[intf].bind((ip, 9000))
-            listen_sockets[intf].listen(5)
 
     while True:
         readable, writable, exceptional = select.select(listen_sockets.values(),
