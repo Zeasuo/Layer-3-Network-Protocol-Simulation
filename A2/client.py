889b08c41da54d4ea81d55ff26b0f84e5e759aa7
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-20 23:38:12 -0400

    s

--- A2/client.py
+++ A2/client.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     port = 9000
     initialize_msg = b'hello'
     socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     # bind the ip address to the socket with port
     socket.bind((ip_address, port))
     socket.sendto(initialize_msg, ("255.255.255.255", 9000))
