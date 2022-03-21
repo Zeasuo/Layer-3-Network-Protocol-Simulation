10f15561e1d07f7b297f78d86d0e5b5e7862ddf1
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-21 03:37:55 -0400

    s

--- A2/router.py
+++ A2/router.py
@@ -1,11 +1,29 @@
 import socket
 import netifaces as ni
+import select
 
-
+forwarding_table = {}
 if __name__ == "__main__":
+
+    # initializing sockets for each interface other than loopback
     listen_sockets = {}
     interfaces = ni.interfaces()
+    output_sockets = []
     for intf in interfaces:
         if intf != 'lo':
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['addr']
-            print(ip)
+            listen_sockets[intf] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            listen_sockets[intf].bind((ip, 9000))
+            listen_sockets[intf].listen(5)
+
+    while True:
+        readable, writable, exceptional = select.select(listen_sockets.values(),
+                                                        [],
+                                                        [])
+        for s in readable:
+            data, address = s.recvfrom(1024)
+            print(data, address)
+
+
+
+
