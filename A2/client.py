fae39ac36e53fdcac12e2d41b7184b81a80f1480
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-21 05:03:58 -0400

    s

--- A2/client.py
+++ A2/client.py
@@ -1,5 +1,6 @@
 import getopt, sys
 import socket
+import netifaces as ni
 
 ip_address = None
 
@@ -7,11 +8,13 @@ long_options = ["Initialize", "Send", "Output ="]
 if __name__ == "__main__":
     ip_address = sys.argv[1]
     port = 9000
+    subnet_address = ip_address[:len(ip_address)-3] + '255'
+    print(subnet_address)
     initialize_msg = b'hello'
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,socket.IPPROTO_UDP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     # bind the ip address to the socket with port
     s.bind((ip_address, port))
-    s.sendto(initialize_msg, ("255.255.255.255", 9000))
+    s.sendto(initialize_msg, (subnet_address, 9000))
     s.close()
 
--- A2/simpleTopology.py
+++ A2/simpleTopology.py
@@ -22,10 +22,10 @@ class SimpleTopo(Topo):
 
         # Add hosts and switches
         r1 = self.addHost('r1', ip='10.1.0.1/24')
-        h1 = self.addHost('h1', ip='10.1.0.251')
-        h2 = self.addHost('h2', ip='10.1.0.252')
-        h3 = self.addHost('h3', ip="10.100.0.251")
-        h4 = self.addHost('h4', ip="10.100.0.252")
+        h1 = self.addHost('h1', ip='10.1.0.251/24')
+        h2 = self.addHost('h2', ip='10.1.0.252/24')
+        h3 = self.addHost('h3', ip="10.100.0.251/24")
+        h4 = self.addHost('h4', ip="10.100.0.252/24")
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
