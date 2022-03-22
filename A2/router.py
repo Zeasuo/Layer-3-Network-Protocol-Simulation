3cbd3972b6f5bb6d88a5611be554896066251df6
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-22 01:59:51 -0400

    s

--- A2/router.py
+++ A2/router.py
@@ -69,5 +69,5 @@ if __name__ == "__main__":
                 print(data)
                 destination = data['destination']
                 port = data['port']
-                client_connections[(destination, int(port))].send(data)
+                client_connections[(destination, int(port))].send(received)
 
