b99a6f0f380bbcc0a2ffb8a70e914a7cf9ee8675
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-21 20:06:24 -0400

    s

--- A2/router.py
+++ A2/router.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
                 forwarding_table[address[0]] = interface_ip
                 s.sendto(str.encode(interface_ip), (address[0], address[1]))
 
-            elif s in end_to_end_sockets:
+            elif s in end_to_end_sockets.values():
                 print("new connection come in")
                 new_connection = s.accept()
                 new_connection.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(ip_to_intf[s.getsockname()[0]]).encode('utf-8'))
