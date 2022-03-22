bb40f964ec981df9e9a920565ec2577144951ee9
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-21 20:39:55 -0400

    s

--- A2/simpleTopology.py
+++ A2/simpleTopology.py
@@ -24,8 +24,8 @@ class SimpleTopo(Topo):
         r1 = self.addHost('r1', ip='10.1.0.1/24')
         h1 = self.addHost('h1', ip='10.1.0.251/24')
         h2 = self.addHost('h2', ip='10.1.0.252/24')
-        h3 = self.addHost('h3', ip="10.100.0.251/24")
-        h4 = self.addHost('h4', ip="10.100.0.252/24")
+        h3 = self.addHost('h3', ip="10.100.0.251/24", defaultRoute='via 10.100.0.1')
+        h4 = self.addHost('h4', ip="10.100.0.252/24", defaultRoute='via 10.100.0.1')
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
