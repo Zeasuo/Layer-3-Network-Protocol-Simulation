98d30b64a65efb68c09e3201d5df44b72e97ac86
Gongzhi Wang <gongzhi.wang@mail.utoronto.ca>
2022-03-24 02:21:09 -0400

    s

--- A2/multiRouter.py
+++ A2/multiRouter.py
@@ -35,15 +35,15 @@ class MultiRouter(Topo):
         s2 = self.addSwitch('s2')
 
         # r2 network hosts
-        r2 = self.addHost('r2', ip='10.200.0.1/24')
-        h5 = self.addHost('h5', ip='10.200.0.251/24')
-        h6 = self.addHost('h6', ip='10.300.0.251/24')
+        r2 = self.addHost('r2', ip='10.101.0.1/24')
+        h5 = self.addHost('h5', ip='10.101.0.251/24')
+        h6 = self.addHost('h6', ip='10.102.0.251/24')
         s3 = self.addSwitch('s3')
         s4 = self.addSwitch('s4')
 
         # r3 network host
-        r3 = self.addHost('r3', ip='10.400.0.1/24')
-        h7 = self.addHost('h7', ip='10.400.0.251/24')
+        r3 = self.addHost('r3', ip='10.103.0.1/24')
+        h7 = self.addHost('h7', ip='10.103.0.251/24')
         s5 = self.addSwitch('s5')
 
         # Add links based on the above diagram
@@ -58,18 +58,18 @@ class MultiRouter(Topo):
         self.addLink(h4, s2)
 
         # r2 network links
-        self.addLink(s3, r2, intfName2='r2-eth0', params2={'ip': '10.200.0.1/24'})
-        self.addLink(s4, r2, intfName2='r2-eth1', params2={'ip': '10.300.0.1/24'})
+        self.addLink(s3, r2, intfName2='r2-eth0', params2={'ip': '10.101.0.1/24'})
+        self.addLink(s4, r2, intfName2='r2-eth1', params2={'ip': '10.102.0.1/24'})
         self.addLink(h5, s3)
         self.addLink(h6, s4)
 
         # r3 network links
-        self.addLink(s5, r3, intfName2='r3-eth0', params2={'ip': '10.400.0.1/24'})
+        self.addLink(s5, r3, intfName2='r3-eth0', params2={'ip': '10.103.0.1/24'})
         self.addLink(h7, s5)
 
         # links between routers
-        self.addLink(r1, r2, intfName1='r1-eth2', intftName2='r2-eth2', params1={'ip': '10.101.0.1/24'}, params2={'ip': '10.101.0.2/24'})
-        self.addLink(r1, r3, intfName1='r1-eth3', intftName2='r3-eth1', params1={'ip': '10.102.0.1/24'}, params2={'ip': '10.102.0.2/24'})
+        self.addLink(r1, r2, intfName1='r1-eth2', intftName2='r2-eth2', params1={'ip': '10.104.0.1/24'}, params2={'ip': '10.104.0.2/24'})
+        self.addLink(r1, r3, intfName1='r1-eth3', intftName2='r3-eth1', params1={'ip': '10.105.0.1/24'}, params2={'ip': '10.105.0.2/24'})
 
 
 topos = {'multiRouter': (lambda: MultiRouter())}
