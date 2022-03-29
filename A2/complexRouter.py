32f30a7db557eb6695a42080035a05f928566e7c
Connor <connor@ConnordeMacBook-Air.local>
2022-03-29 00:16:16 -0400

    get advertise

--- A2/complexRouter.py
+++ A2/complexRouter.py
@@ -47,7 +47,9 @@ def get_advertise():
             print(receiveddata)
             for (key, value) in receiveddata.items():
                 forwarding_table[key] = sourceaddress
-            print("forwarding_table: " + forwarding_table)
+
+            print("forwarding_table:")
+            print(forwarding_table)
         time.sleep(5)
 
 
