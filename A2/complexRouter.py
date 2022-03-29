22efd9b5e953552579c51506a377429692457cad
Connor315 <gao315320130@gmail.com>
2022-03-29 11:12:29 -0400

    update

--- A2/complexRouter.py
+++ A2/complexRouter.py
@@ -7,6 +7,8 @@ import select
 import json
 
 forwarding_table = {}
+
+
 def advertise():
     intfs = ni.interfaces()
     broadcasts = []
@@ -14,16 +16,19 @@ def advertise():
     for intf in intfs:
         if intf != 'lo':
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['addr']
-            broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                      socket.IPPROTO_UDP)
             broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             broadcast.bind((ip, 9001))
             broadcasts.append(broadcast)
-            socket_b_ip[broadcast] = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
+            socket_b_ip[broadcast] = ni.ifaddresses(intf)[ni.AF_INET][0][
+                'broadcast']
 
     while True:
         readable, writable, exceptional = select.select([], broadcasts, [])
         for s in writable:
-            s.sendto(str.encode(json.dumps(forwarding_table)), (socket_b_ip[s], 9002))
+            s.sendto(str.encode(json.dumps(forwarding_table)),
+                     (socket_b_ip[s], 9002))
         time.sleep(5)
 
 
@@ -33,9 +38,11 @@ def get_advertise():
     for intf in intfs:
         if intf != 'lo':
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
-            receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+            receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                    socket.IPPROTO_UDP)
             receive.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            receive.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(intf).encode('utf-8'))
+            receive.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE,
+                               str(intf).encode('utf-8'))
             receive.bind((ip, 9002))
             receive_from.append(receive)
 
@@ -46,7 +53,10 @@ def get_advertise():
             receiveddata = json.loads(sourcedata.decode())
             print(receiveddata)
             for (key, value) in receiveddata.items():
-                forwarding_table[key] = sourceaddress
+                if key not in forwarding_table.keys() \
+                        or (key in forwarding_table.keys() and value[1] + 1 <
+                            forwarding_table[key][1]):
+                    forwarding_table[key] = sourceaddress
 
             print("forwarding_table:")
             print(forwarding_table)
@@ -70,15 +80,24 @@ if __name__ == "__main__":
     for intf in interfaces:
         if intf != 'lo':
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
-            listen_sockets[ip] = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-            listen_sockets[ip].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            listen_sockets[ip].setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(intf).encode('utf-8'))
+            listen_sockets[ip] = socket.socket(socket.AF_INET,
+                                               socket.SOCK_DGRAM,
+                                               socket.IPPROTO_UDP)
+            listen_sockets[ip].setsockopt(socket.SOL_SOCKET,
+                                          socket.SO_REUSEADDR, 1)
+            listen_sockets[ip].setsockopt(socket.SOL_SOCKET,
+                                          socket.SO_BINDTODEVICE,
+                                          str(intf).encode('utf-8'))
             listen_sockets[ip].bind((ip, 9000))
 
             ip2 = ni.ifaddresses(intf)[ni.AF_INET][0]['addr']
-            end_to_end_sockets[ip2] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            end_to_end_sockets[ip2].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            end_to_end_sockets[ip2].setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(intf).encode('utf-8'))
+            end_to_end_sockets[ip2] = socket.socket(socket.AF_INET,
+                                                    socket.SOCK_STREAM)
+            end_to_end_sockets[ip2].setsockopt(socket.SOL_SOCKET,
+                                               socket.SO_REUSEADDR, 1)
+            end_to_end_sockets[ip2].setsockopt(socket.SOL_SOCKET,
+                                               socket.SO_BINDTODEVICE,
+                                               str(intf).encode('utf-8'))
             end_to_end_sockets[ip2].bind((ip2, 9000))
             end_to_end_sockets[ip2].listen(5)
 
@@ -86,7 +105,8 @@ if __name__ == "__main__":
 
             ip_to_intf[ip2] = intf
 
-    input_sockets = list(listen_sockets.values()) + list(end_to_end_sockets.values())
+    input_sockets = list(listen_sockets.values()) + list(
+        end_to_end_sockets.values())
     print(input_sockets)
 
     threading.Thread(target=advertise).start()
@@ -106,7 +126,9 @@ if __name__ == "__main__":
             if s in end_to_end_sockets.values():
                 print("new connection come in")
                 new_connection, client_ip = s.accept()
-                new_connection.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, str(ip_to_intf[s.getsockname()[0]]).encode('utf-8'))
+                new_connection.setsockopt(socket.SOL_SOCKET,
+                                          socket.SO_BINDTODEVICE, str(
+                        ip_to_intf[s.getsockname()[0]]).encode('utf-8'))
                 client_connections[client_ip] = new_connection
                 input_sockets.append(new_connection)
                 output_sockets.append(new_connection)
