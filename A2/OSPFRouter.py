b0e480308b18b8b9a0a442c8aeb442aed3aaf60e
Lily <lily@LilydeMBP.lan>
2022-03-30 21:30:44 -0400

    fr

--- A2/OSPFRouter.py
+++ A2/OSPFRouter.py
@@ -6,9 +6,7 @@ import netifaces as ni
 import select
 import json
 
-SelfRouterIPToNeighboursIP = {}
-something = {}
-forwarding_table = [SelfRouterIPToNeighboursIP, something]
+forwarding_table = {}
 # sockets that should be input or output to
 input_sockets = []
 output_sockets = []
@@ -17,19 +15,66 @@ router_connections = []
 
 
 def get_neighbour():
-    while True:
-        tIntfs = ni.interfaces()
-        selfip = -1
-        neighbours = []
-        for intf in tIntfs:
+    global input_sockets
+    global output_sockets
+    global router_connections
+    tIntfs = ni.interfaces()
+    broadcasts = []
+    receive_from = []
+    socket_b_ip = {}
+    nearby_router = []
+    bip_to_inet = {}
+    for intf in tIntfs:
+        if intf != 'lo':
             ip = ni.ifaddresses(intf)[ni.AF_INET][0]['addr']
-            if intf == 'lo':
-                selfip = ip
-                SelfRouterIPToNeighboursIP[ip] = []
-            else:
-                neighbours.append(ip)
-        SelfRouterIPToNeighboursIP[selfip] = neighbours
-        print(SelfRouterIPToNeighboursIP)
+            broadcast = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                      socket.IPPROTO_UDP)
+            broadcast.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            broadcast.bind((ip, 9001))
+            broadcasts.append(broadcast)
+            socket_b_ip[broadcast] = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
+
+            ip_b = ni.ifaddresses(intf)[ni.AF_INET][0]['broadcast']
+            receive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM,
+                                    socket.IPPROTO_UDP)
+            receive.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            receive.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE,
+                               str(intf).encode('utf-8'))
+            receive.bind((ip_b, 9002))
+            receive_from.append(receive)
+
+            bip_to_inet[ip_b] = ip
+
+    while True:
+        readable, writable, exceptional = select.select(receive_from, broadcasts, [])
+        if readable:
+            for s in readable:
+                sourcedata, sourceAddress = s.recvfrom(1024)
+                if sourceAddress[0] != bip_to_inet[s.getsockname()[0]]:
+                    receivedData = json.loads(sourcedata.decode())
+                    print("Received: from "+sourceAddress[0])
+                    print(receivedData)
+                    for (key, value) in receivedData.items():
+                        if key not in forwarding_table.keys() \
+                                or (key in forwarding_table.keys() and value[1] + 1 <
+                                    forwarding_table[key][1]):
+                            forwarding_table[key] = (sourceAddress[0], value[1] + 1)
+
+                    if sourceAddress[0] not in nearby_router:
+                        new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        new_socket.bind((bip_to_inet[s.getsockname()[0]], 9005))
+                        new_socket.connect((sourceAddress[0], 9000))
+                        input_sockets.append(new_socket)
+                        output_sockets.append(new_socket)
+                        router_connections.append(new_socket)
+                        nearby_router.append(sourceAddress[0])
+        else:
+            for s in writable:
+                s.sendto(str.encode(json.dumps(forwarding_table)), (socket_b_ip[s], 9002))
+
+                print("forwarding_table:")
+                print(forwarding_table)
+                time.sleep(5)
         time.sleep(5)
 
 
@@ -74,7 +119,6 @@ if __name__ == "__main__":
 
     input_sockets = list(listen_sockets.values()) + list(
         end_to_end_sockets.values())
-    print(input_sockets)
 
     threading.Thread(target=get_neighbour).start()
 
